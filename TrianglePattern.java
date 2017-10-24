d96bdd8607d5d42ccda6b2e3d2e745cff04323f1
AsbjornOlling <asbjornolling@gmail.com>
2017-10-24 11:34:58 +0200

    General structure - grid nested array added, and loops in constructor

--- TrianglePattern.java
+++ TrianglePattern.java
@@ -13,16 +13,46 @@
 
 // 1. seeds are planted in row 0
 // 2. growing process starts top down
+// 		loops h times
+// 3. generate graphic using TPViewer
 
 public class TrianglePattern {
-	private int n, h;
+	// declare fields
+	private int h, n;
 	private int[] initial;
+	private int[][] grid = new int[h][n];
+
+	/*
+	public static void main(String args[]) {
+		System.out.println("Hello world!");
+		// zuck shit
+	}
+	// */
 
 	// constructor
+	// main code of the simulation
 	public TrianglePattern(int n, int h, int[] initial) {
+		// put arguments into instance fields
 		h = h;
 		n = n;
 		initial = initial;
+		int nwBlock,nBlock,neBlock;
+		
+		// put initial[] in first row of grid
+		grid[0] = initial;
+
+		// loop through rows, starting with second row
+		for (int i = 1; i < h; i++) {
+
+			// loop through cells in row, starting with first cell
+			for (int j = 0; j < n; j++) {
+				nwBlock = grid[i-1][j-1];
+				nBlock = grid[i-1][j];
+				neBlock = grid[i-1][j+1];
+			} // cells loop
+
+		} // rows loop
+
 	} // constructor
 
 
