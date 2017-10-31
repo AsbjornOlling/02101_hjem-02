7d2605f7031a750f0e81d93f2eaa5a04a3d8b5fa
AsbjornOlling <asbjornolling@gmail.com>
2017-10-31 12:46:21 +0100

    Added problem 2, with basic template

---
+++ MovingPoint.java
@@ -0,0 +1,35 @@
+/*
+ Problem 3 
+ Hjemmeopgave 2 - Indledende Programmering
+ Asbjørn Olling
+*/ 
+
+// a point in 2-dimensional space which moves
+//
+// movement is described by direction and speed
+// angle: 
+// 	positive x: 0 degrees
+// 	positive y: 90 degrees
+// 	range: 0-360
+// speed:
+// 	a non-negative value
+// 	distance in one time unit
+// 	range 0-20.0 (20.0 max speed)
+// position:
+// 	x and y coords
+//
+// two constructors: 
+//  one defaults to 0,0, speed0, dir 90
+
+public class MovingPoint {
+	// makes a point with dir 90, speed 0, at coords 0,0
+	public MovingPoint() {
+		// gogogog
+	} // default constructor
+
+	// makes a point with specific coords, dir and speed
+	public MovingPoint(double x, double y, double direction, double speed) {
+		// GOGOGO
+	}	// specific constructor
+
+} // class
