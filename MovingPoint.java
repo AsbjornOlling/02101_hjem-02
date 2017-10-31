4699cf0970814446d75007b7f1dad87545bb1842
AsbjornOlling <asbjornolling@gmail.com>
2017-10-31 13:49:36 +0100

    First final draft - w/ debug main method

--- MovingPoint.java
+++ MovingPoint.java
@@ -1,7 +1,7 @@
 /*
- Problem 3 
- Hjemmeopgave 2 - Indledende Programmering
- Asbjørn Olling
+// Problem 3 
+// Hjemmeopgave 2 - Indledende Programmering
+// Asbjørn Olling
 */ 
 
 // a point in 2-dimensional space which moves
@@ -17,19 +17,84 @@
 // 	range 0-20.0 (20.0 max speed)
 // position:
 // 	x and y coords
-//
-// two constructors: 
-//  one defaults to 0,0, speed0, dir 90
 
-public class MovingPoint {
+import java.lang.Math;
+import java.awt.geom.Point2D; // class to inherit from
+
+public class MovingPoint extends Point2D.Double {
+	// introduce variables specific to moving points
+	public double direction;
+	public double speed;
+
+	public static void main(String[] a){
+		MovingPoint mp1 = new MovingPoint(1., 2, 180, 2.0);
+
+		System.out.println(mp1);
+		mp1.move(3);
+		System.out.println(mp1);
+
+		mp1.turnBy(-540);
+		System.out.println(mp1);
+
+		mp1.accelerateBy(4.0);
+		mp1.move(2);
+		System.out.println(mp1);
+
+		mp1.accelerateBy(-7.0);
+		mp1.move(2);
+		System.out.println(mp1);
+		mp1.accelerateBy(3.0);
+		mp1.move(1.43);
+		System.out.println(mp1);
+	}
+
 	// makes a point with dir 90, speed 0, at coords 0,0
 	public MovingPoint() {
-		// gogogog
+		x = 0;
+		y = 0;
+		direction = 90;
+		speed = 0;
 	} // default constructor
+
 
 	// makes a point with specific coords, dir and speed
 	public MovingPoint(double x, double y, double direction, double speed) {
-		// GOGOGO
+		this.x = x;
+		this.y = y;
+		this.direction = direction;
+		this.speed = speed;
 	}	// specific constructor
 
+
+	// move the point for a number of time units
+	public void move(double duration) {
+		x += Math.cos(direction) * speed * duration;
+		y += Math.sin(direction) * speed * duration;
+	} // move
+
+
+	// change the angle, make sure it's not over 360
+	public void turnBy(double angle) {
+		direction = (direction + angle) % 360;
+	}	// turnBy
+
+
+	// change the speed
+	public void accelerateBy(double change) {
+		// make the simple speed change
+		speed += change;
+
+		// if speed negative, make sped positive
+		// and change the angle
+		if (speed < 0) {
+			speed = speed * -1;
+			direction = (direction + 180) % 360;
+		} // fi
+	} // accelerateBy
+
+
+	// return text in the format: "[x;y] d s"
+	public String toString() {
+		return "["+x+";"+y+"] "+direction+" "+speed;
+	} // toString
 } // class
